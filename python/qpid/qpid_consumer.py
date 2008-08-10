c55d876325f13cf974e245df1a6ad4458191c71f
Phillip Pearson <pp@myelin.co.nz>
2008-08-10 13:25:39 +1200

    updated qpid receiver to use values from config.txt

--- python/qpid/qpid_consumer.py
+++ python/qpid/qpid_consumer.py
@@ -1,17 +1,21 @@
-import qpid, time
+import qpid, time, sys, os.path
+HERE = os.path.split(sys.argv[0])[0]
+sys.path.insert(0, os.path.join(HERE, '..', 'shared'))
+import config
+conf = config.Config()
 
-conn = qpid.client.Client('localhost', 5672, qpid.spec.load('qpid/specs/amqp.0-8.xml'), vhost='/')
-print conn.start({"LOGIN": "login", "PASSWORD": "password"})
+conn = qpid.client.Client(conf.host, conf.port, qpid.spec.load(os.path.join(HERE, 'amqp.0-8.xml')), vhost='/')
+print conn.start({"LOGIN": conf.user, "PASSWORD": conf.password})
 ch = conn.channel(1)
 print ch.channel_open()
 r = ch.access_request('/data', active=True, read=True, write=True)
 ticket = 0
-ch.exchange_declare(ticket, "tempexch", "direct", durable=False, auto_delete=False)
-ch.queue_declare(queue="tempqueue", durable=False, exclusive=False, auto_delete=False)
-ch.queue_bind(queue="tempqueue", exchange="tempexch", routing_key="tempqueue")
+ch.exchange_declare(ticket, conf.exchange, "direct", durable=False, auto_delete=False)
+ch.queue_declare(queue=conf.queue, durable=False, exclusive=False, auto_delete=False)
+ch.queue_bind(queue=conf.queue, exchange=conf.exchange, routing_key=conf.queue)
 
 while 1:
-    msg = ch.basic_get(queue="tempqueue")
+    msg = ch.basic_get(queue=conf.queue)
     c = msg.content
     if c is not None:
         # handle message now
--- python/shared/config.py
+++ python/shared/config.py
@@ -1,14 +1,22 @@
 import os.path
 
 class Config:
+
+    TYPES = {
+        'port': 'int',
+        'durable': 'bool',
+        }
+    
     def __init__(self):
         fn = os.path.join(os.path.abspath(os.path.split(__file__)[0]), "..", "..", "config.txt")
         print "Reading configuration from %s" % fn
         for line in open(fn).readlines():
             p = line.find("#")
             if p != -1: line = line[:p]
-            k, v = line.split("=", 2)
-            setattr(self, k.strip(), v.strip())
-
-    def get_bool(self, k):
-        return getattr(self, k).lower() in ('1', 'true', 'yes', 'on')
+            k, v = [_.strip() for _ in line.split("=", 2)]
+            typ = Config.TYPES.get(k)
+            if typ == 'int':
+                v = int(v)
+            elif typ == 'bool':
+                v = (v.lower() in ('1', 'true', 'yes', 'on'))
+            setattr(self, k, v)
