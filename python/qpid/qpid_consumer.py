1606bf32073b5ecda913a338ee0f3907f32ba11e
Phillip Pearson <pp@myelin.co.nz>
2008-08-10 13:45:53 +1200

    changed qpid consumer output to look more like amqplib consumer output

--- python/qpid/qpid_consumer.py
+++ python/qpid/qpid_consumer.py
@@ -5,22 +5,32 @@ import config
 conf = config.Config()
 import qpid
 
+print "qpid consumer"
 conn = qpid.client.Client(conf.host, conf.port, qpid.spec.load(os.path.join(HERE, 'amqp.0-8.xml')), vhost='/')
-print conn.start({"LOGIN": conf.user, "PASSWORD": conf.password})
+conn.start({"LOGIN": conf.user, "PASSWORD": conf.password})
 ch = conn.channel(1)
-print ch.channel_open()
+ch.channel_open()
 r = ch.access_request('/data', active=True, read=True, write=True)
 ticket = 0
 ch.exchange_declare(ticket, conf.exchange, "direct", durable=False, auto_delete=False)
 ch.queue_declare(queue=conf.queue, durable=False, exclusive=False, auto_delete=False)
 ch.queue_bind(queue=conf.queue, exchange=conf.exchange, routing_key=conf.queue)
 
+print "using basic.get"
+last = int(time.time())
+n = 0
 while 1:
     msg = ch.basic_get(queue=conf.queue)
     c = msg.content
+    now = int(time.time())
+    if now != last:
+        last = now
+        print n
+        n = 0
     if c is not None:
         # handle message now
-        print c.body
+        #print c.body
+        n += 1
         ch.basic_ack(msg.delivery_tag)
     else:
         time.sleep(1)
